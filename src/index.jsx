cfb6730146fb56a115090ddfd112082b1244a01c
Danil <danil@davletov.net>
2023-10-20 19:05:27 +0300

    breakpoints added

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOMClient from "react-dom/client";
 import { Desktop } from "./screens/Desktop";
+import { Phone } from "./screens/Phone";
 
 const app = document.getElementById("app");
 const root = ReactDOMClient.createRoot(app);
-root.render(<Desktop />);
+root.render(
+  <>
+    <Desktop />
+    <Phone />
+  </>
+);
